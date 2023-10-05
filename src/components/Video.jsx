612c43f206ccf6a7c2a3a8a67829027522f9090b
Michael Geers <michael@geers.tv>
2023-10-06 00:21:59 +0200

    new blogpost: feature highlights

---
+++ src/components/Video.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+export default ({ src, poster }) => {
+  return (
+    <video
+      loop
+      muted
+      controls
+      style={{
+        maxWidth: "100%",
+        height: "auto",
+        marginBottom: "var(--ifm-leading)",
+      }}
+      poster={poster}
+    >
+      <source src={src} type="video/webm" />
+      Your browser does not support the video tag.
+    </video>
+  );
+};
