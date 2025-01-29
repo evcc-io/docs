a63a93e0b27bfa04968040920f4f8fa05ff71bdb
Maschga <delphi@freenet.de>
2025-01-29 19:50:21 +0100

    use `react-dom/client`

--- src/components/SwaggerUIWrapper.jsx
+++ src/components/SwaggerUIWrapper.jsx
@@ -1,5 +1,5 @@
 import React, { useRef, useEffect } from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import SwaggerUI from "swagger-ui-react";
 
 const customCss = `
