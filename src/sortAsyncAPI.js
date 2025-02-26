648ae4c2d14e8b08c70ef14a135f42a2f026c387
Maschga <delphi@freenet.de>
2025-02-26 14:03:29 +0100

    wip

---
+++ src/sortAsyncAPI.js
@@ -0,0 +1,40 @@
+const fs = require("fs");
+const yaml = require("js-yaml");
+const { forEach } = require("lodash");
+
+const filePath = "./static/mqtt-api.yaml";
+
+function sortObject(obj) {
+  return Object.keys(obj)
+    .sort()
+    .reduce((sorted, key) => {
+      sorted[key] = obj[key];
+      return sorted;
+    }, {});
+}
+
+function sortAsyncAPI(filePath) {
+  try {
+    const data = fs.readFileSync(filePath, "utf8");
+    let asyncAPI = yaml.load(data);
+
+    if (asyncAPI.operations) {
+      asyncAPI.operations = sortObject(asyncAPI.operations);
+    }
+
+    if (asyncAPI.components) {
+      ["messages", "parameters", "tags"].forEach((key) => {
+        if (asyncAPI.components[key]) {
+          asyncAPI.components[key] = sortObject(asyncAPI.components[key]);
+        }
+      });
+    }
+
+    fs.writeFileSync(filePath, yaml.dump(asyncAPI, { quotingType: '"' }));
+    console.log("AsyncAPI YAML file successfully sorted and updated.");
+  } catch (error) {
+    console.error("Error processing AsyncAPI YAML file:", error);
+  }
+}
+
+sortAsyncAPI(filePath);
