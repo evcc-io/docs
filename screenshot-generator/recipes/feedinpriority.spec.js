1301f086061838484169a9325c56b89a7fb51629
Michael Geers <michael@geers.tv>
2025-07-17 12:30:05 +0200

    dedicated feedin page; add screenshots

--- screenshot-generator/recipes/feedinpriority.spec.js
+++ screenshot-generator/recipes/feedinpriority.spec.js
@@ -5,7 +5,7 @@ const { start, stop } = require("./utils/evcc");
 const BASE_PATH = "features/screenshots";
 
 test.beforeAll(async () => {
-  await start(["basics.evcc.yaml", "dynamicprice.evcc.yaml"], "password.sql");
+  await start(["basics.evcc.yaml", "feedinpriority.evcc.yaml"], "password.sql");
 });
 test.afterAll(async () => {
   await stop();
@@ -14,36 +14,21 @@ test.afterAll(async () => {
 const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 loop((screenshot) => {
-  test("smart grid charging", async ({ page }) => {
+  test("feedin priority", async ({ page }) => {
     await page.goto("/");
     await page.getByTestId("loadpoint-settings-button").nth(1).click();
     await wait(300);
 
-    await page.locator("#smartCostLimit-1").selectOption("0.2");
+    await page.locator("#smartFeedInPriority-1").selectOption("0.3");
 
     await screenshot(
       page,
-      `${BASE_PATH}/dynamicprice-modal`,
+      `${BASE_PATH}/feedin-priority-modal`,
       "#loadpointSettingsModal_1 .modal-body > .container > div:first-child",
       {
         all: 50,
         top: 110,
         right: 70,
-      },
-    );
-  });
-
-  test("charging plan", async ({ page }) => {
-    await page.goto("/");
-    await page.locator("[data-testid=charging-plan] button").first().click();
-    await wait(300);
-    await page.getByTestId("static-plan-energy").selectOption("30");
-    await screenshot(
-      page,
-      `${BASE_PATH}/dynamicprice-plan`,
-      "#chargingPlanModal_1 .modal-content",
-      {
-        all: 20,
       },
     );
   });
